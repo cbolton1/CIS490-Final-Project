2761aacfccefdb4315733c2febea371d67df3994
Chris <57192910+cbolton1@users.noreply.github.com>
2021-04-19 18:35:38 -0400

    Added specific kmeans clustering with fewer features

--- CIS490 Final Project/CIS490_Final_Project.py
+++ CIS490 Final Project/CIS490_Final_Project.py
@@ -33,8 +33,106 @@ print(data_norm.head(5))
 test_set = data_norm.head(5000) #how much of the dataset is being used in order to save time during testing
 
 
+#K-Means Clustering (4 features, mintemp, maxtemp, rainfall, evaporation)
+x = test_set.iloc[:,[1,2,3,4]].values
+kmeans = KMeans(init='random', n_init=10, n_clusters = 3) #number of clusters being used
+y_kmeans = kmeans.fit_predict(x)
+print(y_kmeans)
+kmeans.cluster_centers_
 
-#K-Means Clustering
+#Test cluster numbers for error 1 through kmax
+
+kmax = 15
+
+Error =[]
+for i in range(2, kmax+1):
+    kmeans = KMeans(n_clusters = i).fit(x)
+    kmeans.fit(x)
+    Error.append(kmeans.inertia_)
+
+#Plot elbow method results
+plt.plot(range(2, kmax+1), Error)
+plt.title('Elbow method')
+plt.xlabel('No of clusters')
+plt.ylabel('Error')
+plt.show()
+
+#Silhouette test
+sil =[]
+for k in range(2, kmax+1):
+  kmeans = KMeans(n_clusters = k).fit(x)
+  labels = kmeans.labels_
+  sil.append(silhouette_score(x, labels, metric = 'euclidean'))
+
+plt.plot(range(2, kmax+1), sil)
+plt.title('Silhouette Method')
+plt.xlabel('No of clusters')
+plt.ylabel('Silhouette Score')
+plt.show()
+
+
+#Plot kmeans results in scatterplot
+plt.figure(figsize=(10,10))
+plt.scatter(x[:,0],x[:,1],s=20,marker='o',c=y_kmeans,cmap='rainbow')
+plt.show()
+
+
+
+
+
+#K-Means Clustering (features Humidity9am, Humidity3pm, Pressure9am, Pressure3pm, RainTomorrow_No, RainTomorrow_Yes)
+x = test_set.iloc[:,[8,9,10,11,18,19]].values
+kmeans = KMeans(init='random', n_init=10, n_clusters = 3) #number of clusters being used
+y_kmeans = kmeans.fit_predict(x)
+print(y_kmeans)
+kmeans.cluster_centers_
+
+#Test cluster numbers for error 1 through kmax
+
+kmax = 15
+
+Error =[]
+for i in range(2, kmax+1):
+    kmeans = KMeans(n_clusters = i).fit(x)
+    kmeans.fit(x)
+    Error.append(kmeans.inertia_)
+
+#Plot elbow method results
+plt.plot(range(2, kmax+1), Error)
+plt.title('Elbow method')
+plt.xlabel('No of clusters')
+plt.ylabel('Error')
+plt.show()
+
+#Silhouette test
+sil =[]
+for k in range(2, kmax+1):
+  kmeans = KMeans(n_clusters = k).fit(x)
+  labels = kmeans.labels_
+  sil.append(silhouette_score(x, labels, metric = 'euclidean'))
+
+plt.plot(range(2, kmax+1), sil)
+plt.title('Silhouette Method')
+plt.xlabel('No of clusters')
+plt.ylabel('Silhouette Score')
+plt.show()
+
+
+#Plot kmeans results in scatterplot
+plt.figure(figsize=(10,10))
+plt.scatter(x[:,0],x[:,1],s=20,marker='o',c=y_kmeans,cmap='rainbow')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+#K-Means Clustering (largest set)
 x = test_set.iloc[:,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]].values
 kmeans = KMeans(init='random', n_init=10, n_clusters = 2) #number of clusters being used
 y_kmeans = kmeans.fit_predict(x)
