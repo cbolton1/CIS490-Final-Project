bc71db83422b004a2a5920394243dd974d9fdf0f
Chris <57192910+cbolton1@users.noreply.github.com>
2021-04-19 16:51:25 -0400

    Added a testing set with only the first 5000 rows of the dataset to speeed up testing.

--- CIS490 Final Project/CIS490_Final_Project.py
+++ CIS490 Final Project/CIS490_Final_Project.py
@@ -7,24 +7,25 @@ import seaborn as sns
 #Allow display of all columns for debugging
 pd.set_option('display.max_columns',None)
 
-#Read the dataset into a dataframe, remove the date
+#Read the dataset into a dataframe, remove most of the qualitative data
 df = pd.read_csv("weatherAUS.csv")
-df = df.drop(columns=['Date'],inplace=False)
+df = df.drop(columns=['Date','Location','WindGustDir','WindDir9am','WindDir3pm'],inplace=False)
 
 print(df.head(5))
 
 #Create dummy columns for categorical data
 data_norm = df.copy()
-data_norm = pd.get_dummies(data_norm, columns=["Location","WindGustDir","WindDir9am","WindDir3pm","RainToday","RainTomorrow"])
+data_norm = pd.get_dummies(data_norm, columns=["RainToday","RainTomorrow"])
 
 #Fill NaN values with means
 data_norm = data_norm.where(pd.notna(data_norm),data_norm.mean(),axis="columns")
 
 print(data_norm.head(5))
 
+test_set = data_norm.head(5000) #how much of the dataset is being used in order to save time during testing
 
 #K-Means Clustering
-x = data_norm.iloc[:,[1,2,3]].values
+x = test_set.iloc[:,[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]].values
 kmeans = KMeans(n_clusters = 3) #number of clusters being used
 y_kmeans = kmeans.fit_predict(x)
 print(y_kmeans)
@@ -45,6 +46,6 @@ plt.ylabel('Error')
 plt.show()
 
 #Plot kmeans results in scatterplot
-plt.figure(figsize=(20,20))
-plt.scatter(x[:,0],x[:,1],c=y_kmeans,cmap='rainbow')
+plt.figure(figsize=(10,10))
+plt.scatter(x[:,0],x[:,1],s=20,marker='o',c=y_kmeans,cmap='rainbow')
 plt.show()
