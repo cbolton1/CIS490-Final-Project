e0a78a6085e48f8f124a22658a207cecc3c1c2b1
Chris <57192910+cbolton1@users.noreply.github.com>
2021-04-19 12:26:47 -0400

    KMeans may be working, need to figure out how to visualize

--- CIS490 Final Project/CIS490_Final_Project.py
+++ CIS490 Final Project/CIS490_Final_Project.py
@@ -1,6 +1,7 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+import seaborn as sns
 
 #Allow display of all columns for debugging
 pd.set_option('display.max_columns',None)
@@ -15,11 +16,16 @@ print(df.head(5))
 data_norm = df.copy()
 data_norm = pd.get_dummies(data_norm, columns=["Location","WindGustDir","WindDir9am","WindDir3pm","RainToday","RainTomorrow"])
 
+#Fill NaN values with means
+data_norm = data_norm.where(pd.notna(data_norm),data_norm.mean(),axis="columns")
+
 print(data_norm.head(5))
 
 #K-Means Clustering
-kmeans = KMeans(3)
-clusters = kmeans.fit_predict(data_norm)
-labels = pd.DataFrame(clusters)
+kmeans = KMeans(n_clusters=3)
+y = kmeans.fit_predict(data_norm)
+data_norm['Cluster'] = y
+
+print(data_norm.head())
 
 
